c9dace4723ced1d671978f60e57443e14e2343a8
saman batool <samanbatool08@gmail.com>
2019-10-22 15:57:05 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  array = [1, 2, 3]
 end
+
+square_array(array)
+
+def new_square_array(numbers)
+  numbers = [9, 10, 16, 25]
+end
+
+square_array(new_square_array)
